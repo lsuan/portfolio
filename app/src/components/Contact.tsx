8ce40b49abed7220b67956ce309736d89718db9f
Lee Suan <lsuan@Lees-MacBook-Pro.local>
2022-10-12 05:15:13 -0700

    removing form action attr

--- app/src/components/Contact.tsx
+++ app/src/components/Contact.tsx
@@ -66,7 +66,7 @@ function Contact() {
           </div>
         </div>
         <div className="contact-form p-3 card col-12 col-md-6">
-          <form id="contact-form" name="contact" className="d-flex d-flex flex-column justify-content-between" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit} action="success">
+          <form id="contact-form" name="contact" className="d-flex d-flex flex-column justify-content-between" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
             <input name="bot-field" className="d-none" />
             <input type="hidden" name="form-name" value="contact" />
             <div className="inputs">
